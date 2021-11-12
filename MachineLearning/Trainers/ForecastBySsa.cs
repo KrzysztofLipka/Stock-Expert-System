233568d8874f1f812be89f595d7f77e21c694f6e
Krzysztof.Lipka <krzysztoflipka94@gmail.com>
2021-11-12 18:50:24 +0100

    adding features to forecast by SSA

--- MachineLearning/Trainers/ForecastBySsa.cs
+++ MachineLearning/Trainers/ForecastBySsa.cs
@@ -9,13 +9,14 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using ScottPlot;
 
 namespace MachineLearning.Trainers
 {
     public class ForecastBySsa
     {
 
-        string connectionString = "todo move to config";
+        string connectionString = "";
         public void Predict(string dataPath, string modelOuptutPath, string companyName) {
 
 
@@ -38,28 +39,70 @@ namespace MachineLearning.Trainers
 
             //DatabaseSource dbSource = new DatabaseSource(SqlClientFactory.Instance, connectionString, sqlCommand);
 
-            IDataView dataFromDb = LoadDataFromDb(context, companyName);
+            int numberOfRows;
+            IDataView dataFromDb = LoadDataFromDb(context, companyName,out numberOfRows);
+
+            
+
+            Console.WriteLine(numberOfRows);
+
+            //double testFraction = Math.Round(365 /(double)numberOfRows,4);
+
+            
+
+            //double test2 = numberOfRows * testFraction;
+
+            List<StockDataPointInput> dataFromDbToArray = context.Data.CreateEnumerable<StockDataPointInput>(dataFromDb, reuseRowObject: false).ToList();
+
+            var splitIndex = numberOfRows - 365;
+
+            List<StockDataPointInput> trainList;
+            List<StockDataPointInput> testList;
+
+            SplitList(dataFromDbToArray, out trainList, out testList, splitIndex);
+
+            //var testData = context.Data.LoadFromEnumerable<StockDataPointInput>(test);
+
+            var trainSet = context.Data.LoadFromEnumerable<StockDataPointInput>(trainList); 
+
+            var testSet = context.Data.LoadFromEnumerable<StockDataPointInput>(testList);
+
+
+
+
+            //var split = context.Data
+            //   .TrainTestSplit(dataFromDb, testFraction: testFraction
+            //   );
+
+            //var trainSet = context.Data
+            //   .CreateEnumerable<StockDataPointInput>(split.TrainSet, reuseRowObject: false);
+
+
+            //var testSet = context.Data
+            //    .CreateEnumerable<StockDataPointInput>(split.TestSet, reuseRowObject: false);
 
             //------------------
 
-            var data = context.Data.LoadFromTextFile<NbpData>(dataPath,
-                hasHeader: false,
-                separatorChar: ',');
-            var lastElementInFile = data.Preview(100000).RowView.Last();
-           
+            //var data = context.Data.LoadFromTextFile<NbpData>(dataPath,
+            //    hasHeader: false,
+            //    separatorChar: ',');
+            //var lastElementInFile = data.Preview(100000).RowView.Last();
 
-            Console.WriteLine(data.Preview().RowView);
-            Console.WriteLine(nameof(NbpData.ExchangeRate));
+
+            //Console.WriteLine(data.Preview().RowView);
+            //Console.WriteLine(nameof(NbpData.ExchangeRate));
 
             var pipeline = context.Forecasting.ForecastBySsa(
                 "Forecast",
                "ClosingPrice",
-               windowSize: 5,
-               seriesLength: 10,
-               trainSize: 215,
-               horizon: 4);
+               windowSize: trainList.Count/4,
+               seriesLength: trainList.Count,
+               trainSize: trainList.Count,
+               horizon: 365);
 
-            var model = pipeline.Fit(dataFromDb);
+            //var model = pipeline.Fit(dataFromDb);
+            var model = pipeline.Fit(trainSet);
+
 
             var forecastingEngine = model.CreateTimeSeriesEngine<StockDataPointInput, NbpForecastOutput>(context);
 
@@ -69,6 +112,21 @@ namespace MachineLearning.Trainers
             {
                 Console.WriteLine(forecast);
             }
+
+            var resAsList = forecasts.Forecast;
+
+            double[] forecastsArray = Array.ConvertAll(resAsList, x => (double)x);
+
+            double[] xs = new double[] { 1, 2, 3, 4, 5 };
+            double[] ys = new double[] { 1, 4, 9, 16, 25 };
+
+            double [] y = DataGen.Consecutive(365);
+            double [] x1 = ConvertInputListToPlotArray(testList);
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.AddScatter(y, x1);
+            plt.AddScatter(y, forecastsArray);
+            plt.SaveFig("console.png");
 
             forecastingEngine.CheckPoint(context, modelOuptutPath);
 
@@ -90,7 +148,7 @@ namespace MachineLearning.Trainers
 
             var testData = context.Data.LoadFromEnumerable<StockDataPointInput>(test);
 
-            this.Evaluate(testData, model, context);
+            this.Evaluate(testSet, model, context);
 
             Console.ReadLine();
         }
@@ -151,16 +209,57 @@ namespace MachineLearning.Trainers
 
         }
 
-        private IDataView LoadDataFromDb(MLContext context, string companyName) {
+        private IDataView LoadDataFromDb(MLContext context, string companyName, out int numberOfRows) {
             DatabaseLoader loader = context.Data.CreateDatabaseLoader<StockDataPointInput>();
             //string sqlCommand = "select ClosingPrice from dbo.HistoricalPrices where CompanyId = 1 order by PriceId";
             string sqlCommand = $"EXEC dbo.GetClosingPrices @CompanyName = '{companyName}'";
+            string sqlCommand2 = $"EXEC dbo.GetClosingPricesWithMaxDate @CompanyName = '{companyName}', @MaxDate = '11-11-2019'";
 
-            DatabaseSource dbSource = new DatabaseSource(SqlClientFactory.Instance, connectionString, sqlCommand);
+            //EXEC dbo.GetClosingPricesWithMaxDate @CompanyName = 'AAPL', @MaxDate = '11-11-2020'
+
+            DatabaseSource dbSource = new DatabaseSource(SqlClientFactory.Instance, connectionString, sqlCommand2);
 
             IDataView dataFromDb = loader.Load(dbSource);
 
+            numberOfRows = dataFromDb.Preview(20000).RowView.Length;
+
             return dataFromDb;
+        }
+
+        private void SplitList(
+            List<StockDataPointInput> inputList,
+            out List<StockDataPointInput> listOne,
+            out List<StockDataPointInput> listTwo,
+            int splitIndex
+            )
+        {
+            var res1 = new List<StockDataPointInput>();
+            var res2 = new List<StockDataPointInput>();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i < splitIndex) {
+                    res1.Add(inputList[i]);
+                } else {
+                    res2.Add(inputList[i]);
+                };
+                    
+            }
+
+            listOne = res1;
+            listTwo = res2;
+        }
+
+        private double[] ConvertInputListToPlotArray(List<StockDataPointInput> list) {
+
+            var res = new List<double>();
+            foreach (var item in list)
+            {
+                res.Add((double)item.ClosingPrice);
+            }
+
+            return res.ToArray();
+            
         }
 
     }
